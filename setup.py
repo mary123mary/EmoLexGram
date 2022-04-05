8f00c2f5ce6215a1606c61dec22dab5ade18c97d
mary123mary <marrycatherine22@gmail.com>
2022-04-06 01:03:39 +0530

    init

---
+++ setup.py
@@ -0,0 +1,34 @@
+"""Setup for the emoLexGram package."""
+
+
+import setuptools
+
+
+with open('README.md') as f:
+    README = f.read()
+
+setuptools.setup(
+    author="Moksha Thisarani",
+    author_email="moksha.util@gmail.com",
+    name='EmoLexGram',
+    license="MIT",
+    description='EmoLexGram is a python package for text-based emotion classification',
+    version='v0.0.1',
+    long_description=README,
+    url='https://github.com/MOKSHA22/EmoLexGram',
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    install_requires=["pandas", "nltk", "requests"],
+    classifiers=[
+        'Development Status :: 1 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Research',
+    ],
+)
